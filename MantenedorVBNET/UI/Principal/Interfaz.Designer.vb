7fe4994e9cc4e6ef13666525834d3180d855f2a9
camiloalvarez98 <camiloalvarezduarte@gmail.com>
2022-12-06 00:05:17 -0300

    comit2

---
+++ MantenedorVBNET/UI/Principal/Interfaz.Designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Interfaz
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Interfaz))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.DataGridViewImageColumn1 = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.DataGridViewImageColumn2 = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.Editar = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.Eliminar = New System.Windows.Forms.DataGridViewImageColumn()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(361, 175)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingresar/Actualizar artículo:"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(9, 137)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(338, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Actualizar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(9, 107)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(338, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Ingresar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(98, 81)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(249, 20)
+        Me.DateTimePicker1.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 81)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(95, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Fecha de ingreso: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(174, 45)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Código bodega: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(174, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Stock mínimo: "
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(265, 45)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(82, 20)
+        Me.TextBox4.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(265, 19)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(82, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(81, 45)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(87, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(81, 19)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(87, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Valor: "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Descripción: "
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Editar, Me.Eliminar})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 224)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(571, 180)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.TextBox5)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Location = New System.Drawing.Point(389, 13)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 79)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Consultar  bodega: "
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(9, 47)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(185, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Consultar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(94, 22)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 1
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 25)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(85, 13)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Código artículo: "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(15, 205)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(106, 13)
+        Me.Label7.TabIndex = 3
+        Me.Label7.Text = "Información artículos"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(18, 189)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 13)
+        Me.Label8.TabIndex = 4
+        '
+        'DataGridViewImageColumn1
+        '
+        Me.DataGridViewImageColumn1.HeaderText = "Editar"
+        Me.DataGridViewImageColumn1.Image = Global.UI.My.Resources.Resources.editar__1_
+        Me.DataGridViewImageColumn1.Name = "DataGridViewImageColumn1"
+        '
+        'DataGridViewImageColumn2
+        '
+        Me.DataGridViewImageColumn2.HeaderText = "Eliminar"
+        Me.DataGridViewImageColumn2.Image = Global.UI.My.Resources.Resources.cruz__1_
+        Me.DataGridViewImageColumn2.Name = "DataGridViewImageColumn2"
+        '
+        'Editar
+        '
+        Me.Editar.HeaderText = "Editar"
+        Me.Editar.Image = Global.UI.My.Resources.Resources.editar__1_
+        Me.Editar.Name = "Editar"
+        '
+        'Eliminar
+        '
+        Me.Eliminar.HeaderText = "Eliminar"
+        Me.Eliminar.Image = Global.UI.My.Resources.Resources.cruz
+        Me.Eliminar.Name = "Eliminar"
+        '
+        'Interfaz
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(598, 421)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Interfaz"
+        Me.Text = "Mantenedor CAS-CHILE"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Editar As DataGridViewImageColumn
+    Friend WithEvents Eliminar As DataGridViewImageColumn
+    Friend WithEvents DataGridViewImageColumn1 As DataGridViewImageColumn
+    Friend WithEvents DataGridViewImageColumn2 As DataGridViewImageColumn
+End Class
---
+++ MantenedorVBNET/UI/Principal/Interfaz.vb
@@ -0,0 +1,195 @@
+﻿Imports Entidades
+Imports Negocio
+Public Class Interfaz
+    Private Sub Interfaz_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListarArticulos()
+        Button2.Visible = False
+        Label8.Visible = False
+    End Sub
+
+
+#Region "Botones"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim value As Integer = Nothing, value2 As Integer = Nothing, value3 As Integer = Nothing
+        If String.IsNullOrEmpty(TextBox1.Text) OrElse String.IsNullOrEmpty(TextBox2.Text) OrElse String.IsNullOrEmpty(TextBox3.Text) OrElse String.IsNullOrEmpty(TextBox4.Text) Then
+            MessageBox.Show("Rellene los campos de artículo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+            If Integer.TryParse(TextBox3.Text.Trim(), value) AndAlso Integer.TryParse(TextBox2.Text.Trim(), value2) AndAlso Integer.TryParse(TextBox4.Text.Trim(), value3) Then
+                Dim bodega As New ClsBodega()
+                bodega.Codigo_Bodega = Convert.ToInt32(TextBox4.Text)
+                Dim objBodegaLn = New ClsBodegaLn()
+                Dim respuesta As Integer
+                respuesta = objBodegaLn.ValidarBodega(bodega)
+                If respuesta = 0 Then
+                    MessageBox.Show("El código de bodega ingresado no existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+                Else
+                    Dim dialogResult As DialogResult = MessageBox.Show("Está a punto de crear el artículo " & TextBox1.Text & vbLf & "¿Desea continuar?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+                    If dialogResult = DialogResult.Yes Then
+                        Dim articulo As New ClsArticulo With {
+                        .Descripcion = TextBox1.Text,
+                        .Valor = Convert.ToInt32(TextBox2.Text),
+                        .StockMinimo = Convert.ToInt32(TextBox3.Text),
+                        .Codigo_Bodega = Convert.ToInt32(TextBox4.Text),
+                        .Fecha_ingreso = DateTimePicker1.Value
+                        }
+                        Dim objArticuloLn = New ClsArticuloLn()
+                        objArticuloLn.Create(articulo)
+                        MessageBox.Show("El Articulo " + articulo.Descripcion + " fue ingresado correctamente", "Transacción exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        TextBox1.Clear()
+                        TextBox2.Clear()
+                        TextBox3.Clear()
+                        TextBox4.Clear()
+                        ListarArticulos()
+                    End If
+                End If
+            Else
+                MessageBox.Show("Los campos Valor, Stock mínimo y Código deben ser valores numéricos (enteros)", "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim value As Integer = Nothing, value2 As Integer = Nothing, value3 As Integer = Nothing
+        If String.IsNullOrEmpty(TextBox1.Text) OrElse String.IsNullOrEmpty(TextBox2.Text) OrElse String.IsNullOrEmpty(TextBox3.Text) OrElse String.IsNullOrEmpty(TextBox4.Text) Then
+            MessageBox.Show("Rellene los campos de artículo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+            If Integer.TryParse(TextBox3.Text.Trim(), value) AndAlso Integer.TryParse(TextBox2.Text.Trim(), value2) AndAlso Integer.TryParse(TextBox4.Text.Trim(), value3) Then
+                Dim bodega As New ClsBodega()
+                bodega.Codigo_Bodega = Convert.ToInt32(TextBox4.Text)
+                Dim objBodegaLn = New ClsBodegaLn()
+                Dim respuesta As Integer
+                respuesta = objBodegaLn.ValidarBodega(bodega)
+                If respuesta = 0 Then
+                    MessageBox.Show("El código de bodega ingresado no existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+                Else
+                    Dim dialogResult As DialogResult = MessageBox.Show("Está a punto de actualizar un artículo ¿Desea continuar?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+                    If dialogResult = DialogResult.Yes Then
+                        Dim articulo As New ClsArticulo With {
+                            .Codigo = Convert.ToInt32(Label8.Text),
+                            .Descripcion = TextBox1.Text,
+                            .Valor = Convert.ToInt32(TextBox2.Text),
+                            .StockMinimo = Convert.ToInt32(TextBox3.Text),
+                            .Codigo_Bodega = Convert.ToInt32(TextBox4.Text),
+                            .Fecha_ingreso = DateTimePicker1.Value
+                        }
+                        Dim objArticuloLn = New ClsArticuloLn()
+                        objArticuloLn.Update(articulo)
+                        MessageBox.Show("El Artículo (Código: " & Label8.Text & ") fue actualizado correctamente", "Actualización exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        TextBox1.Clear()
+                        TextBox2.Clear()
+                        TextBox3.Clear()
+                        TextBox4.Clear()
+                        Button2.Visible = False
+                        ListarArticulos()
+                    End If
+                End If
+            Else
+                MessageBox.Show("Los campos Valor, Stock mínimo y Código deben ser valores numéricos (enteros)", "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+            End If
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim value As Integer = Nothing
+        If String.IsNullOrEmpty(TextBox5.Text) Then
+            MessageBox.Show("Rellene los campos de artículo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+            If Integer.TryParse(TextBox5.Text.Trim(), value) Then
+                Consultar()
+            Else
+                MessageBox.Show("El campo 'Código artículo' debe ser un valor numérico (entero)", "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+            End If
+        End If
+    End Sub
+
+
+#End Region
+
+
+#Region "Acciones"
+    Private Sub Consultar()
+
+        Dim articulo As New ClsArticulo()
+        articulo.Codigo = Convert.ToInt32(TextBox5.Text)
+        Dim objArticuloLn = New ClsArticuloLn()
+        Dim articuloResp As New ClsArticulo()
+        articuloResp = objArticuloLn.Consultar(articulo)
+        If articuloResp Is Nothing Then
+            MessageBox.Show("Articulo (Código: " & TextBox5.Text & ") no existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBox4.Clear()
+        Else
+            MessageBox.Show("Articulo " & articuloResp.Descripcion & " (Código: " + TextBox5.Text & ")" & vbLf & "Nombre bodega: " + articuloResp.Nombre_Bodega, "Consulta exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            TextBox4.Clear()
+        End If
+
+    End Sub
+
+    Private Sub ListarArticulos()
+        Dim objNegocio As New ClsArticuloLn
+        DataGridView1.DataSource = objNegocio.Index.Tables("Articulos")
+        DataGridView1.ReadOnly = True
+        DataGridView1.AllowUserToAddRows = False
+        DataGridView1.AllowUserToDeleteRows = False
+
+
+    End Sub
+
+    Private Sub Eliminard(e As DataGridViewCellEventArgs)
+        Dim articulo As New ClsArticulo
+        Dim articuloResp As New ClsArticulo
+        articulo.Codigo = Convert.ToInt32(DataGridView1.Rows(e.RowIndex).Cells("Codigo").Value.ToString())
+        Dim objArticuloLn As New ClsArticuloLn
+        articuloResp = objArticuloLn.Read(articulo)
+        Dim dialogResult As DialogResult = MessageBox.Show("¿Estás seguro de eliminar el artículo " & articuloResp.Descripcion & " (Código: " + articuloResp.Codigo.ToString() & ")?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+        If dialogResult = DialogResult.Yes Then
+            objArticuloLn.Delete(articulo)
+            MessageBox.Show("El Artículo (Código " + articulo.Codigo.ToString() + ") fue eliminado correctamente", "Transacción exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ListarArticulos()
+        End If
+    End Sub
+
+    Private Sub Cargar_Datos_Update(e As DataGridViewCellEventArgs)
+        Dim articulo As New ClsArticulo
+        articulo.Codigo = Convert.ToInt32(DataGridView1.Rows(e.RowIndex).Cells("Codigo").Value.ToString())
+        Label8.Text = articulo.Codigo.ToString()
+        Dim objArticuloLn As New ClsArticuloLn
+        Dim articuloResp As New ClsArticulo
+        articuloResp = objArticuloLn.Read(articulo)
+        TextBox1.Text = articuloResp.Descripcion.ToString()
+        TextBox2.Text = articuloResp.Valor.ToString()
+        TextBox3.Text = articuloResp.StockMinimo.ToString()
+        TextBox4.Text = articuloResp.Codigo_Bodega.ToString()
+        DateTimePicker1.Value = articuloResp.Fecha_ingreso
+
+    End Sub
+
+#End Region
+
+
+
+#Region "DataSet"
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        Try
+
+            If DataGridView1.Columns(e.ColumnIndex).Name = "Editar" Then
+                Button2.Visible = True
+                Cargar_Datos_Update(e)
+            ElseIf DataGridView1.Columns(e.ColumnIndex).Name = "Eliminar" Then
+                Eliminard(e)
+
+            End If
+
+        Catch ex As Exception
+            Throw
+        End Try
+    End Sub
+
+#End Region
+
+
+
+
+
+
+End Class
